eb713e8384e06616e4c456f92ab85bac72ffdb2f
chencs0603 <1021823177@qq.com>
2017-12-12 19:43:25 +0800

    认证策略示例

---
+++ strategy/src/main/java/personal/chencs/practice/shiro/StaticRealm.java
@@ -0,0 +1,33 @@
+package personal.chencs.practice.shiro;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StaticRealm extends AuthorizingRealm {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        String username = token.getPrincipal().toString();
+        String password = new String((char[]) token.getCredentials());
+
+        logger.info("username:{}, password:{}", username, password);
+
+        if (!"chencs0603".equals(username)) throw new UnknownAccountException("username not exists");
+
+        if (!"123456".equals(password)) throw new IncorrectCredentialsException("error password");
+
+        AuthenticationInfo info = new SimpleAuthenticationInfo(username, password, getName());
+        return info;
+    }
+}
