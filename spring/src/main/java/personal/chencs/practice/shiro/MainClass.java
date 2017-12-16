d5aa06fc697957f406e8d44d9a27f2eea95e4f44
chencs0603 <1021823177@qq.com>
2017-12-16 15:42:32 +0800

    spring整合示例：去掉多余的import语句

--- spring/src/main/java/personal/chencs/practice/shiro/MainClass.java
+++ spring/src/main/java/personal/chencs/practice/shiro/MainClass.java
@@ -5,7 +5,6 @@ import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
